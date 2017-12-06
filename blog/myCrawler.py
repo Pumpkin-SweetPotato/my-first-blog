7d9d615e7454043d316904c519970403a9c742cf
EC2 Default User <ec2-user@ip-172-31-19-188.ap-northeast-2.compute.internal>
2017-12-06 15:14:06 +0000

    aws-version

--- blog/myCrawler.py
+++ blog/myCrawler.py
@@ -1,5 +1,5 @@
 import requests
-from urllib import parse
+import urllib
 from bs4 import BeautifulSoup
 
 def spider(item_name):
--- testproject/settings.py
+++ testproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$-4@+#466icm_f22nk-3d(*p34d#n&4gg3-h6ib^3q2h88yhn9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com','.elasticbeanstalk.com']
 
 
 # Application definition
